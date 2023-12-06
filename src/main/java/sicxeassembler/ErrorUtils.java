7d8e3fb1d3d1d8de5ea1dfcd3acc84708fb2a42a
Ryan Blue <ryanzblue@gmail.com>
2023-12-06 02:01:14 -0500

    Add error message generation function

---
+++ src/main/java/sicxeassembler/ErrorUtils.java
@@ -0,0 +1,30 @@
+package sicxeassembler;
+
+import java.text.ParseException;
+
+public class ErrorUtils {
+  /**
+   * Prints a formatted error
+   *
+   * @param fileName The file the error is from
+   * @param lineNumber The line number the error occurs on
+   * @param inputString The specific text that caused the error
+   * @param error The ParseException that contains the message and location
+   */
+  public static void error(
+      String fileName, int lineNumber, String inputString, ParseException error) {
+    String output =
+        fileName
+            + ":"
+            + lineNumber
+            + ":"
+            + "error: "
+            + error.getMessage()
+            + "\n"
+            + inputString
+            + "\n"
+            + " ".repeat(Math.max(0, error.getErrorOffset()))
+            + "^";
+    System.err.println(output);
+  }
+}
